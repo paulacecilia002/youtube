d7b52bdfeeb80dbfd7712054bcb18c9ab1ee7663
Paula Cecilia <paulacecilia4700@gmail.com>
2026-08-11 17:39:19 -0300

    adicionando botoes

---
+++ lib/Home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Youtube"),
+        actions: [
+          IconButton( icon: Icon(Icons.videocam),
+      onPressed:(){
+            print("açao videocam");
+      },
+          ),
+          IconButton( icon: Icon(Icons.search),
+            onPressed:(){
+              print("açao pesquisa");
+            },
+          ),
+          IconButton( icon: Icon(Icons.account_circle),
+            onPressed:(){
+              print("açao conta");
+            },
+          ),
+        ],
+      ) ,
+      body: Container(),
+    );
+  }
+}
